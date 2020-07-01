7bed2230a115fc2b8dc6fbb2ab97d3a974b5d987
Abdelrhman Safwat <Abdelrhman.Safwat@gmail.com>
2020-07-01 20:18:15 +0400

    remove unknown command

--- scripts/node.ps1
+++ scripts/node.ps1
@@ -1,7 +1,6 @@
  #Node.Js
 choco install -y nodejs-lts # Node.js LTS, Recommended for most users
 # choco install -y nodejs # Node.js Current, Latest features
-Update-Environment-Path
 choco install -y python2 # Node.js requires Python 2 to build native modules
 # npm
 npm install --global --production npm-windows-upgrade
