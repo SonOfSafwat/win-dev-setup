90c38ec2c53373b894b18e96db8417db7dcff38a
Abdelrhman Safwat <Abdelrhman.Safwat@gmail.com>
2020-07-01 18:13:24 +0400

    fix(filenames): enable case sensitive file names

---
+++ scripts/git.ps1
@@ -0,0 +1,3 @@
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal /NoAutoCrlf /NoShellIntegration /NoGuiHereIntegration /NoShellHereIntegration /SChannel'"
+choco install -y git-credential-winstore
+choco isntall -y Git-Credential-Manager-for-Windows
