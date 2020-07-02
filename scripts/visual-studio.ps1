a68120f8747485856ec71de4a20c0702e6d6980b
Abdelrhman Safwat <Abdelrhman.Safwat@gmail.com>
2020-07-02 15:30:07 +0400

    remove command params causing issue while installing

--- scripts/visual-studio.ps1
+++ scripts/visual-studio.ps1
@@ -3,7 +3,7 @@
 # visualstudio2017community
 # visualstudio2017professional
 # visualstudio2017enterprise
-choco install -y visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+choco install -y visualstudio2017community
 Update-SessionEnvironment #refreshing env due to Git install
 
 #--- UWP Workload and installing Windows Template Studio ---
@@ -14,6 +14,5 @@ choco install -y visualstudio2017-workload-universal
 choco install -y visualstudio2017-workload-manageddesktop
 choco install -y visualstudio2017-workload-nativedesktop
 
-choco install -y sql-server-management-studio
 Write-Host ""
 Update-SessionEnvironment
